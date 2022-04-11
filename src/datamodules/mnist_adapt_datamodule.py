42d753bab68e955040165a81b729daa37b23e7ce
Eoghan <eoghan@deepseek.ai>
2022-04-11 15:53:49 +0100

    add test data (a hold-back target set)

--- src/datamodules/mnist_adapt_datamodule.py
+++ src/datamodules/mnist_adapt_datamodule.py
@@ -8,46 +8,17 @@ from albumentations.pytorch.transforms import ToTensorV2
 from pytorch_lightning import LightningDataModule
 from torch.utils import data
 from torch.utils.data import ConcatDataset, DataLoader, Dataset, Subset, random_split
-from tqdm import tqdm
-from urllib import request
-import pathlib
-import pickle
-import gzip
-import numpy as np
+
 
 from pytorch_adapt.datasets import DataloaderCreator, get_mnist_mnistm
-from pytorch_adapt.validators import IMValidator
+from pytorch_adapt.validators import MultipleValidators, AccuracyValidator, IMValidator
 from pytorch_adapt.frameworks.utils import filter_datasets
 
-from src.datamodules.datasets.mnist_detection_dataset import MnistDetectionDataset
 
 import src.datamodules.mnist_generate.mnist as mnist
 import src.datamodules.mnist_generate.generate_data as generate_data
 
-class Collater:
-    # https://shoarora.github.io/2020/02/01/collate_fn.html
-    def __call__(self, batch):
-        return tuple(zip(*batch))
-
-
 class MnistAdaptDataModule(LightningDataModule):
-    """
-    MnistDetectionDataModule for Mnist object detection.
-
-    A DataModule implements 5 key methods:
-        - prepare_data (things to do on 1 GPU/TPU, not on every GPU/TPU in distributed mode)
-        - setup (things to do on every accelerator in distributed mode)
-        - train_dataloader (the training dataloader)
-        - val_dataloader (the validation dataloader(s))
-        - test_dataloader (the test dataloader(s))
-
-    This allows you to share a full dataset without explaining how to download,
-    split, transform and process the data.
-
-    Read the docs:
-        https://pytorch-lightning.readthedocs.io/en/latest/extensions/datamodules.html
-    """
-
     def __init__(
         self,
         data_dir: str = "data/mnistm/",
@@ -62,36 +33,10 @@ class MnistAdaptDataModule(LightningDataModule):
         # it also ensures init params will be stored in ckpt
         self.save_hyperparameters(logger=False)
 
-        self.transforms = A.Compose(
-            [
-                A.Normalize(),
-                A.HorizontalFlip(p=0.5),
-                A.Blur(blur_limit=3, p=0.15),
-                ToTensorV2(),
-            ],
-            bbox_params=A.BboxParams(format="pascal_voc", label_fields=["category_ids"]),
-        )
-
-        self.notransforms = A.Compose(
-            [
-                A.Normalize(),
-                ToTensorV2(),
-            ],
-            bbox_params=A.BboxParams(format="pascal_voc", label_fields=["category_ids"]),
-        )
-
-        self.collater = Collater()
-
-        # self.dims is returned when you call datamodule.size()
-        self.dims = (1, 300, 300)
         self.data_train: Optional[Dataset] = None
         self.data_val: Optional[Dataset] = None
         self.data_test: Optional[Dataset] = None
         self.dataloaders = None
-
-    @property
-    def num_classes(self) -> int:
-        return self.hparams.num_classes
 
     def prepare_data(self):
         """Download data if needed. This method is called only from a single GPU.
@@ -107,9 +52,12 @@ class MnistAdaptDataModule(LightningDataModule):
             datasets = get_mnist_mnistm(["mnist"], ["mnistm"], folder=self.hparams.data_dir, download=False)
             dc = DataloaderCreator(batch_size=self.hparams.batch_size, num_workers=self.hparams.num_workers)
             validator = IMValidator()
+            # validator = MultipleValidators([AccuracyValidator(), IMValidator()])
             self.dataloaders = dc(**filter_datasets(datasets, validator))
             self.data_train = self.dataloaders.pop("train")
             self.data_val = list(self.dataloaders.values())
+            test_dc = DataloaderCreator(batch_size=self.hparams.batch_size, num_workers=self.hparams.num_workers, val_names=["target_test"])
+            self.data_test = test_dc(target_test=datasets['target_val'])['target_test']
             return            
 
     def train_dataloader(self):
@@ -117,3 +65,6 @@ class MnistAdaptDataModule(LightningDataModule):
 
     def val_dataloader(self):
         return self.data_val
+
+    def test_dataloader(self):
+        return self.data_test
